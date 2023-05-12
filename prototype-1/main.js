1ffc50d7d8225cbb34f45d8cddd7f5d1e704c652
Dominic Schweizer <dominic.schweizer@bfh.ch>
2023-05-12 12:37:16 +0200

    updates timeline

--- prototype-1/main.js
+++ prototype-1/main.js
@@ -6,22 +6,32 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 let polygon1;
 let polygon2;
-
+let popUp;
+let T = 1500;
+let event;
+setTimeout(() => {
+  popUp = L.popup()
+    .setLatLng([46.94863, 7.45164])
+    .setContent("AUSBRUCH Gaswolke")
+    .openOn(map);
+}, T);
 setTimeout(() => {
   polygon1 = L.polygon([
     [46.94863, 7.45164],
     [46.94587, 7.44254],
     [46.94387, 7.44554],
   ]).addTo(map);
-}, 1000);
+}, T + 1000);
+setTimeout(() => {
+  popUp = L.popup()
+    .setLatLng([46.94587, 7.44254])
+    .setContent("IN EINER STUNDE \nPerson über 75 Jahre")
+    .openOn(map);
+}, T + 1000);
 setTimeout(() => {
   polygon2 = L.polygon([
     [46.94863, 7.45164],
     [46.92987, 7.41154],
     [46.92287, 7.42654],
   ]).addTo(map);
-}, 2000);
-var popup = L.popup()
-  .setLatLng([46.94587, 7.44254])
-  .setContent("IN EINER STUNDE \nPerson über 75 Jahre")
-  .openOn(map);
+}, T + 2000);
